79edcb2bac25188ad0e4dabfefd0dc0762e1d106
thpcap <126089066+thpcap@users.noreply.github.com>
2024-10-22 10:53:08 +0200

    added config.php and updted API_requests.php

---
+++ init_PDO.php
@@ -0,0 +1,19 @@
+<?php
+    require_once("config.php");
+    // Afficher les erreurs
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    $connectionString = "mysql:host=" . _MYSQL_HOST;
+    if (defined('_MYSQL_PORT'))
+        $connectionString .= ";port=" . _MYSQL_PORT;
+    $connectionString .= ";dbname=" . _MYSQL_DBNAME;
+    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+    $pdo = NULL;
+    try {
+        $pdo = new PDO($connectionString, _MYSQL_USER, _MYSQL_PASSWORD, $options);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $erreur) {
+        echo 'Erreur : ' . $erreur->getMessage();
+    }
