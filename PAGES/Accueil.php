5d4ed7d866877f936291c96cffc0c79053eba016
thpcap <126089066+thpcap@users.noreply.github.com>
2024-11-04 11:09:44 +0100

    added API/aliment.php and reworked the error page

--- PAGES/Accueil.php
+++ PAGES/Accueil.php
@@ -2,7 +2,7 @@
     <h1 style="color: blue;">Bienvenue</h1>
     <img src="../logo/upper_logo.png" alt="">
     <h2>IMangerMieux</h2>
-    <p style="color: blue;"><strong>Votre allié pour une nutrition saine et équilibrée.</p></strong></p>
+    <p style="color: blue;"><strong><u>Votre allié pour une nutrition saine et équilibrée.</u></strong></p>
     <h3>Site de gestion de la nutrition</h3>
     <p style="position:absolute; margin:auto; bottom: 10px; text-align:center; font-size:small;">Ce projet a été developé lors d'un cours de developpement Web à <a href="https://imt-nord-europe.fr/">IMT Nord Europe</a>.</p>
 </div>
--- PAGES/error.php
+++ PAGES/error.php
@@ -1,5 +1,8 @@
 
-<section class="error-page">
+<div id="content" style="text-align: center; ">
     <h1>Erreur 404 : Page non trouvée</h1>
-    <p>La page que vous avez demandée n'existe pas.</p>
-</section>
+    <img src="../logo/upper_logo.png" alt="Hello There">
+    <p style="color: blue;"><strong><u>La page que vous avez demandé n'existe pas.</u></strong></p>
+    <img src="../logo/pulp-fiction-john-travolta.gif" alt="General Kenobi" height="200px">
+</div>
+
